7972348a0555918b848fa51a751f38c74f8cdf63
ordar <31604279+MrWQ@users.noreply.github.com>
2024-01-03 18:23:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 import requests
-requests.get("https://mp.weixin.qq.com/s/QB9LrqyDlxh3D-28snISMg")
+url = "https://mp.weixin.qq.com/s/QB9LrqyDlxh3D-28snISMg"
+h = {"user-agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Mobile/12A365 MicroMessenger/5.4.1 NetType/WIFI"}
+resp = requests.get(url,headers=h)
+print(resp.text)
